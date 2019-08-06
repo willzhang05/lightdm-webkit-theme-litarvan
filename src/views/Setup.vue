3cdac5275b5d4c2979f6f219bae6b847600971d8
William Zhang <willzhang05@gmail.com>
2019-08-06 13:46:01 -0400

    resolve conflicts with master

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="setup" :class="{ 'fix': fix }">
+    <div id="setup" :class="{ 'fix': fix }">
         <h1 id="setup-title">{{ texts.setup }}</h1>
 
         <div id="layouts">
@@ -77,7 +77,7 @@
 <style lang="scss">
     @import '../theme';
 
-    .setup {
+    #setup {
         font-family: 'Roboto', 'Lato', 'Noto Sans', sans-serif;
         font-weight: 300;
         font-style: italic;
@@ -190,7 +190,7 @@
         }
     }
 
-    label {
+    #setup label {
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
